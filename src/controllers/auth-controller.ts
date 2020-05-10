2372574467b1471f1998bd091d34d65b4dfd504a
gebhartn <nicholas.gebhart@gmail.com>
2020-05-10 15:14:32 -0700

    chore: auth controller

---
+++ src/controllers/auth-controller.ts
@@ -0,0 +1,27 @@
+import { PrismaClient, User } from '@prisma/client'
+import { Request, Response } from 'express'
+import { sign } from 'jsonwebtoken'
+import { compare } from 'bcryptjs'
+
+const prisma = new PrismaClient()
+
+export const AuthController = {
+  login: async (req: Request, res: Response) => {
+    const { username, password } = req.body
+
+    if (!(username && password)) res.send(401)
+
+    let user: User | null
+
+    user = await prisma.user.findOne({ where: { username } })
+
+    if (!user) return res.status(400)
+
+    if (!(await compare(password, user.password))) return res.send(401)
+
+    const token =
+      user && sign({ id: user.id }, process.env.SECRET!, { expiresIn: '1h' })
+
+    res.send({ token })
+  },
+}
